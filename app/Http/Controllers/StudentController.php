a0838df4e59c311c8c9c050b4172ac941bb8a881
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-11-05 15:48:51 +0700

    Update student function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
 use App\Http\Resources\ClassResource;
 use App\Http\Resources\StudentResource;
 use App\Models\Classes;
@@ -34,6 +35,23 @@ class StudentController extends Controller
     public function store(StoreStudentRequest $request)
     {
         Student::create($request->validated());
+        return redirect()->route('students.index');
+    }
+
+    public function edit(Student $student)
+    {
+        $classes = ClassResource::collection(Classes::all());
+
+        return inertia('Student/Edit', [
+            'student' => StudentResource::make($student),
+            'classes' => $classes
+        ]);
+    }
+
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        $student->update($request->validated());
+
         return redirect()->route('students.index');
     }
 }
