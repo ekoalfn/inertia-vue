d71d41774b1032340b5bd146b0f9e3708c021209
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-11-05 15:45:25 +0700

    Create student function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Resources\ClassResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -17,5 +20,20 @@ class StudentController extends Controller
                 'students' => $students
             ]
         );
+    }
+
+    public function create()
+    {
+        $classes = ClassResource::collection(Classes::all());
+
+        return inertia('Student/Create', [
+            'classes' => $classes
+        ]);
+    }
+
+    public function store(StoreStudentRequest $request)
+    {
+        Student::create($request->validated());
+        return redirect()->route('students.index');
     }
 }
